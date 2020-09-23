e4b3c8d4dc23b768cbeb204cf46cfcc842d73d5f
SongsShao <1175715363@qq.com>
2020-09-23 14:48:03 +0800

    修改定时器

--- src/page/my/index.js
+++ src/page/my/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react'
 import { Icon } from 'antd'
 import indexTitle from '../../assets/indexTitle.png'
@@ -5,7 +6,7 @@ import indexTitle from '../../assets/indexTitle.png'
 function Index() {
   return (
     <div style={{ position: 'relative' }}>
-      暂未开发
+      <iframe style={{ width: '100%', height: document.documentElement.clientHeight - 100.5, border: 0 }} key src='http://192.168.6.130:18001/'></iframe>
     </div>
   )
 }
